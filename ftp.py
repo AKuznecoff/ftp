c18db8b758ff1f73e7dce4b656ce92971403486e
Kuznecoff <artem98kuznecov@mail.ru>
2018-05-12 12:33:08 +0500

    open and close done

--- ftp.py
+++ ftp.py
@@ -1,4 +1,7 @@
-import socket, getpass, select
+import socket, getpass, select, re
+
+
+CONNECTION = None
 
 
 def main():
@@ -9,36 +12,67 @@ def main():
 			process_command(command.split())
 
 
-def process_command(input):
+def process_command(args):
 	try:
-		COMMANDS[input[0]](input[1:])
+		COMMANDS[args[0]](args[1:])
 	except KeyError:
 		print('invalid command')
 
 
 def connect(addr):
-	with socket.create_connection((addr[0], 21)) as conn:
-		#print('login for {}'.format(addr), end='')
-		login = input('login for {}'.format(addr[0]))
-		conn.send('USER {}\r\n'.format(login).encode())
-		#print('password for {}'.format(login), end='')
-		#password = input('password for {}'.format(login))
-		password = getpass.getpass()
+	global CONNECTION
+	if CONNECTION is not None:
+		print('Connection is open already')
+		return
+	if len(addr) != 1:
+		print('incorrect number of arguments, expected 1, but was {}'.format(len(addr)))
+	CONNECTION = socket.create_connection((addr[0], 21))
+	#CONNECTION.settimeout(2)
+	print(read_answer(), end='')
+	#print('login for {}'.format(addr), end='')
+	login = input('login for {} '.format(addr[0]))
+	CONNECTION.send('USER {}\r\n'.format(login).encode())
+	print(read_answer(), end='')
+	print('password for {} '.format(login), end='')
+	password = input('password for {} '.format(login))
+	#password = getpass.getpass('Password: ')
+	CONNECTION.send('PASS {}\r\n'.format(password).encode())
+	print(read_answer(), end='')
 
 
-def close_connection(conn):
+def close_connection(args):
+	if len(args) > 0:
+		print('incorrect number of arguments, expected 0, but was {}'.format(len(args)))
+	if CONNECTION is None:
+		print('connection is not open')
+		return
+	CONNECTION.close()
+	print('connection closed')
 
 
-def read_answer(conn):
-	r, _, _ = select.select(conn, None, None, 2)
+def read_answer():
+	r, _, _ = select.select([CONNECTION], [], [], 2)
 	answ = b''
-	while r:
-		line = r.recv(1024)
-		print(line.decode())
+	if r:
+		line = r[0].recv(1024)
+		#print(line.decode())
 		answ += line
+	return answ.decode()
+	# result = []
+	# s = CONNECTION.makefile()
+	# answ = s.readline()
+	# while re.match('\d{3} ', answ) == None:
+	# 	#answ = CONNECTION.recv(1024)
+	#
+	# 	# if not answ:
+	# 	# 	break
+	# 	result.append(answ)
+	# 	answ = s.readline()
+	# result.append(answ)
+	# return ''.join(result)
 
 
-COMMANDS = {'open': connect}
+COMMANDS = {'open': connect, 'close': close_connection}
 
 
 if __name__ == '__main__':
